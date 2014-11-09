8ce0f428aa9e0852b01da63d6ff43bfea7ce84c7
root <root@atylerPi-idd.(none)>
2014-11-09 23:03:25 +0000

    door lock on post

--- server.js
+++ server.js
@@ -1,4 +1,22 @@
 //set up server
+var gpio = require('pi-gpio');
+function doorLock(value) {
+	gpio.open(16, "output", function(err) {
+		if (value == '1') {
+			gpio.write(16, 1, function() {
+				gpio.close(16);
+			}
+		} else {
+			gpio.write(16, 0, function() {
+				gpio.close(16);
+			}
+		}
+};
+
+
+
+
+
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -22,6 +40,7 @@ router.get('/', function(req, res) {
 router.route('/util') 
 	.post(function(req, res) {
 		var postVal = req.body.value;
+		doorLock(postVal);
 	res.json({ message: 'value recieved'});
 	})
 	
