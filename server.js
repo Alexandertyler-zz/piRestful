3f560d240c41cf1b1cb952764e3fc122090359f2
root <root@atylerPi-idd.(none)>
2014-11-09 02:20:55 +0000

    post and get

--- server.js
+++ server.js
@@ -10,9 +10,27 @@ var port = process.env.PORT || 8080;
 
 var router = express.Router();
 
+router.use(function(req, res, next) {
+	console.log('Middleware');
+	next();
+});
+
 router.get('/', function(req, res) {
 	res.json({ message: 'it works'});
 });
+
+router.route('/util') 
+	.post(function(req, res) {
+		var postVal = req.body.value;
+	res.json({ message: 'value recieved'});
+	})
+	
+	.get(function(req, res) {
+		var getVal = "placeholder";
+		res.json({ value: getVal });
+	});
+
+
 
 app.use('/api', router);
 
